520cf3fc810269976b3ccaf23d33435afd222431
Amr204 <140981183+Amr204@users.noreply.github.com>
2024-09-26 18:09:28 +0300

    login is ready .Amr

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,59 +1,172 @@
-import 'package:e_commerce_app/widget/support_widget.dart';
-import 'package:flutter/material.dart';
+// import 'package:e_commerce_app/widget/support_widget.dart';
+// import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+// class Login extends StatefulWidget {
+//   const Login({super.key});
+
+//   @override
+//   State<Login> createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         margin: EdgeInsets.only(top: 80, left: 20, right: 30),
+//         decoration: BoxDecoration(
+//           color: const Color.fromARGB(255, 202, 51, 40),
+//           borderRadius: BorderRadius.circular(40)
+
+//         ),
+//         padding:EdgeInsets.all(20),
+
+//         child: Column(
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//               Center(
+//                 child: Text("login",
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 60,
+//                   )
+
+//                 )),
+
+//               ]),
+//             Container(
+//               padding: EdgeInsets.only(top: 50),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(50)
+//                 ),
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.circular(30)
+//                   ),
+//                   child: Image.asset("images/login.png",
+//                   ),
+//                 ),
+//               ))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:e_commerce_app/widget/support_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  State<LogIn> createState() => _LogInState();
 }
 
-class _LoginState extends State<Login> {
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 80, left: 20, right: 30),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 202, 51, 40),
-          borderRadius: BorderRadius.circular(40)
-
-        ),
-        padding:EdgeInsets.all(20),
-
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Center(
-                child: Text("login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 60,
-                  )
-                  
-                  
-                )),
-              
-              ]),
-            Container(
-              padding: EdgeInsets.only(top: 50),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50)
-                ),
-                child: Container(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 40),
+          child: Column(
+            children: [
+              Image.asset("images/login.png"),
+              Text(
+                "Sign in",
+                style: AppWidget.semiboldTextFieldStyle(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Please enter the details beloew to\n                     continue",
+                style: AppWidget.lightTextFieldStyle(),
+              ),
+              Text(
+                "Email",
+                style: AppWidget.semiboldTextFieldStyle(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  padding: EdgeInsets.only(left: 20),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30)
-                  ),
-                  child: Image.asset("images/login.png",
-                  ),
+                      color: Color(0xfff4f5f9),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Email"),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Password",
+                style: AppWidget.semiboldTextFieldStyle(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                    color: Color(0xfff4f5f9),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: "Password"),
                 ),
-              ))
-          ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30,),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  padding: EdgeInsets.all(18),
+                  decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.circular(10)),
+                  child: Center(child: Text("LOGIN",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.bold))),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text("Don't have an account? ",style: AppWidget.lightTextFieldStyle(),),
+                  Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+              ],),
+            ],
+          ),
         ),
       ),
     );
